6b394c59646369f8dce5e4956f8c54bd2cc453b6
Ares <user35@mail.ru>
2025-12-14 18:40:08 +0300

    builds localy

--- cmake/IncreaseMinor.cmake
+++ cmake/IncreaseMinor.cmake
@@ -4,7 +4,11 @@ set(AV_VERSION "0.1.0")
 #string(TIMESTAMP NOW)
 
 # Include the module
-list(APPEND CMAKE_MODULE_PATH "${CMAKE_SOURCE_DIR}/cmake")
-include(AutoVersion.cmake)
+#temp solution is to step some backwards to dir
+list(APPEND CMAKE_MODULE_PATH "${DIR}/cmake")
+message(STATUS "IM cm cdir IS ${CMAKE_CURRENT_SOURCE_DIR}")
+message(STATUS "MP is  ${DIR}/cmake")
+message(STATUS "MPa is  ${CMAKE_MODULE_PATH}")
+include(AutoVersion)
 
-increment_and_save_min_ver()
+increment_and_save_min_ver(${DIR})
